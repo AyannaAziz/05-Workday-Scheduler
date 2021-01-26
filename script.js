3501b43e0cff36b163ff5ebe4addc5b1b1da9fbe
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-01-26 12:50:02 -0500

    initial file setup and outline of code reqs

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 
 // incorporate style classes with JS 
 
+// Reference currentDay to display current date and time on calendar 
+// Us
+// come up for way to call momant js
 
+console.log(moment().hour());
